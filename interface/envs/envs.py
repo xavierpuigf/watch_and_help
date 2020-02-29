small object list; teleport; change return calculation; fix a bug for checking prediction of grab action

interface/envs/envs.py
@@ -114,7 +114,7 @@ class UnityEnvWrapper:
         self.offset_cameras = self.comm.camera_count()[1]
         characters = ['Chars/Female1', 'Chars/Male1']
         for i in range(self.num_agents):
-            self.comm.add_character(characters[i])#, position=[-1, 0, -9])
+            self.comm.add_character(characters[i], position=[-1, 0, -7])
 
         graph = self.get_graph()
         self.rooms = [(node['class_name'], node['id']) for node in graph['nodes'] if node['category'] == 'Rooms']
@@ -147,7 +147,7 @@ class UnityEnvWrapper:
         characters = ['Chars/Female1', 'Chars/Male1']
         for i in range(self.num_agents):
 
-            self.comm.add_character(characters[i])#, position=[-1, 0, -9])
+            self.comm.add_character(characters[i], position=[-1, 0, -7])
 
         graph = self.get_graph()
         self.rooms = [(node['class_name'], node['id']) for node in graph['nodes'] if node['category'] == 'Rooms']
@@ -282,7 +282,7 @@ class UnityEnvWrapper:
                                                        file_name_prefix=self.file_name_prefix,
                                                        image_synthesis=['normal', 'seg_inst', 'seg_class'])
         else:
-            success, message = self.comm.render_script(script_list, recording=False, gen_vid=False)
+            success, message = self.comm.render_script(script_list, recording=False, gen_vid=False, processing_time_limit=20)
         if not success:
             print('action failed:', message)
             # ipdb.set_trace()
@@ -450,7 +450,7 @@ class UnityEnv:
         #is_done = is_close
         is_done = False
         if is_close:
-            reward += 5
+            reward += 1 #5
         info = {'dist': dist, 'done': is_done, 'reward': reward}
         return reward, is_close, info
 
@@ -469,14 +469,17 @@ class UnityEnv:
 
         done = False
         if self.task_type == 'find':
+            id2node = {node['id']: node for node in graph['nodes']}
             grabbed_obj = [id2node[edge['to_id']]['class_name'] for edge in graph['edges'] if
                            'HOLDS' in edge['relation_type']]
+            print(grabbed_obj)
             reward, is_close, info = self.distance_reward(graph, self.goal_find_spec)
             if visible_ids is not None:
 
                 # Reward if object is seen
-                if len(set(self.goal_find_spec).intersection([node[0] for node in visible_ids])) > 0:
-                    reward += 0.5
+                if self.level > 0:
+                    if len(set(self.goal_find_spec).intersection([node[0] for node in visible_ids])) > 0:
+                        reward += 0.5
 
                 if len(set(self.goal_find_spec).intersection(grabbed_obj)) > 0.:
                     reward += 100.
@@ -652,7 +655,7 @@ class UnityEnv:
             # #self.agents[self.system_agent_id].reset(graph, task_goal, seed=self.system_agent_id)
             # #self.history_observations = [torch.zeros(1, 84, 84) for _ in range(self.len_hist)]
             # #self.history_observations = [torch.zeros(1, 84, 84) for _ in range(self.len_hist)]	        if graph is None:
-            if True:
+            if False:
                 self.unity_simulator.comm.fast_reset(self.env_id)
             else:
                 self.unity_simulator.reset(self.env_id, self.init_graph)
@@ -879,11 +882,13 @@ class UnityEnv:
             action_str = self.get_action_command(my_agent_action)
             if action_str is not None:
                 action_dict[1] = action_str
+            print(action_dict)
             dict_results = self.unity_simulator.execute(action_dict)
             self.num_steps += 1
             obs, info = self.get_observations()
 
             # pdb.set_trace()
+            print(info[-1][2])
             reward, done, info = self.reward(visible_ids=info[-1][2], graph=info[1])
             dict_results['finished'] = done
             reward = torch.Tensor([reward])
@@ -1257,7 +1262,8 @@ class UnityEnv:
 
             id2node = {node['id']: node for node in graph['nodes']}
             visible_objects = [object_id for object_id in visible_objects if
-                               self.graph_helper.object_dict.get_id(id2node[object_id]['class_name']) != 0]
+                               self.graph_helper.object_dict.get_id(id2node[object_id]['class_name']) != 0]# and 
+                               # id2node[object_id]['class_name'] == 'wineglass']
 
             if self.level == 0:
                 visible_objects = [object for object in visible_objects if id2node[object]['category'] != 'Rooms']

rl/main2.py
@@ -188,10 +188,10 @@ def main():
         history_locations = []
         for step in range(args.num_steps):
             # Sample actions
-            with torch.no_grad():
-                value, action, action_log_prob, recurrent_hidden_states = actor_critic.act(
-                    {kob: ob[step] for kob, ob in rollouts.obs.items()}, rollouts.recurrent_hidden_states[step],
-                    rollouts.masks[step], epsilon=epsilon)
+            # with torch.no_grad():
+            value, action, action_log_prob, dist_entropy, recurrent_hidden_states = actor_critic.act(
+                {kob: ob[step] for kob, ob in rollouts.obs.items()}, rollouts.recurrent_hidden_states[step],
+                rollouts.masks[step], epsilon=epsilon)
 
             # Obser reward and next obs
             #pdb.set_trace()
@@ -237,7 +237,7 @@ def main():
                 [[0.0] if 'bad_transition' in info.keys() else [1.0]
                  for info in infos])
             rollouts.insert(obs, recurrent_hidden_states, action,
-                            action_log_prob, value, reward, masks, bad_masks)
+                            action_log_prob, dist_entropy, value, reward, masks, bad_masks)
             print(step, reward[0].item(), done[0].item())
             total_num_steps += 1
             done_float = done.astype(np.float32)

utils_rl_agent.py
@@ -59,7 +59,7 @@ class GraphHelper():
 
     def get_action_affordance_map(self, current_task=None, id2node=None):
         dir_path = os.path.dirname(os.path.realpath(__file__))
-        with open(f'{dir_path}/dataset/object_info.json', 'r') as f:
+        with open(f'{dir_path}/dataset/object_info_small.json', 'r') as f:
             content = json.load(f)
 
         n_actions = len(self.actions)
@@ -109,12 +109,18 @@ class GraphHelper():
                                       current_task[0].keys() if t.split('_')[0] not in ['holds', 'sit', 'turnOn']]
                         ids_goal2 = np.array([self.object_dict.get_id(obj_name) for obj_name in obj_names2])
                         self.obj1_affordance[action_id, ids_goal2] = 1
+        # self.obj1_affordance[:,self.object_dict.get_id('kitchencounterdrawer')] = 0
+        self.obj1_affordance[self.action_dict.get_id('open'),self.object_dict.get_id('kitchencounterdrawer')] = 0
+        self.obj1_affordance[self.action_dict.get_id('close'),self.object_dict.get_id('kitchencounterdrawer')] = 0
+        self.obj1_affordance[self.action_dict.get_id('walktowards'),self.object_dict.get_id('kitchencounterdrawer')] = 0
+        self.obj1_affordance[self.action_dict.get_id('walktowards'),self.object_dict.get_id('character')] = 0
+        self.obj1_affordance[:,id_no_obj] = 0
 
 
     def get_objects(self):
         dir_path = os.path.dirname(os.path.realpath(__file__))
 
-        with open(f'{dir_path}/dataset/object_info_rl.json', 'r') as f:
+        with open(f'{dir_path}/dataset/object_info_small.json', 'r') as f:
             content = json.load(f)
         objects = []
         for obj in content.values():
@@ -137,9 +143,8 @@ class GraphHelper():
             if node['category'] == 'Rooms':
                 assert(node['class_name'] in self.rooms)
 
-        # if level > 0:
-        #
-        ids = [node['id'] for node in graph['nodes'] if node['category'] == 'Rooms'] + ids
+        if level > 0:
+            ids = [node['id'] for node in graph['nodes'] if node['category'] == 'Rooms'] + ids
         ids = [idi for idi in ids if idi != character_id]
         ids = list(set(ids))
         id2node = {node['id']: node for node in graph['nodes']}
@@ -236,7 +241,10 @@ def can_perform_action(action, o1, o1_id, agent_id, graph):
     
     # if 'walk' not in action and 'turn' not in action:
     #     return None
-    close_edge = len([edge['to_id'] for edge in graph['edges'] if edge['from_id'] == agent_id and edge['to_id'] == o1 and edge['relation_type'] == 'CLOSE']) > 0
+    close_edge = len([edge['to_id'] for edge in graph['edges'] if edge['from_id'] == agent_id and edge['to_id'] == o1_id and edge['relation_type'] == 'CLOSE']) > 0
+    if action == 'grab':
+        print(agent_id, o1_id, close_edge)
+
 
     if (action in ['grab', 'open', 'close']) and not close_edge:
         return None
@@ -261,6 +269,8 @@ def can_perform_action(action, o1, o1_id, agent_id, graph):
             action = 'putin'
         elif 'SURFACES' in id2node[o1_id]['properties']:
             action = 'putback'
+    if action.startswith('walk'):
+        action = 'walkto'
     action_str = f'[{action}] {obj2_str} {obj1_str}'.strip()
     #print(action_str)
     print(action_str)
@@ -321,6 +331,7 @@ def update_probs(log_probs, i, actions, object_classes, mask_observations, obj1_
         selected_obj1 = torch.gather(object_classes, 1, actions[1].long())
 
         mask = torch.gather(obj1_affordance, 2, selected_obj1.unsqueeze(-2).repeat(1, obj1_affordance.shape[1], 1).long()).squeeze(-1).float().to(log_probs.device)
+        # mask[action_dict.get_id('open'),object_dict.get_id('kitchencounterdrawer') ]= 0
         log_probs = log_probs * mask + (1.-mask) * -inf_val
         
         #print("CLASS OBJ")