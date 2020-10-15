28963e65c4f3b2d2c0d37bec4a8f91cb33175d40
xavierpuigf <xavierpuigf@gmail.com>
2020-10-15 19:42:16 -0400

    watch works

--- watch/predicate/demo_dataset_graph.py
+++ watch/predicate/demo_dataset_graph.py
@@ -158,24 +158,29 @@ def build_graph(nodes, graph_class_names, max_node_length, graph_node_states):
 
 
 def gather_data(args):
-    data_path = 'dataset/watch_data/gather_data_actiongraph.json'
+    data_path = 'dataset/watch_data/gather_data_actiongraph_train.json'
     meta_data_path = 'dataset/watch_data/metadata.json'
     data_path_new_test = 'dataset/watch_data/gather_data_actiongraph_new_test.json'
+    data_path_test = 'dataset/watch_data/gather_data_actiongraph_test.json'
 
     
     with open(data_path_new_test, 'r') as f:
         new_test_data = json.load(f)
         new_test_data = new_test_data['new_test_data']
     
-    if args.inference:
-        data_path = data_path_new_test
-
     if os.path.exists(data_path):
         print('load gather_data, this may take a while...', data_path)
-        with open(data_path, 'r') as f:
+        with open(data_path_test, 'r') as f:
             data = json.load(f)
-            train_data = data['new_test_data']
-            test_data = data['new_test_data']
+            test_data = data['test_data']
+
+        if args.inference:
+            train_data = data['test_data']
+        else:
+            with open(data_path, 'r') as f:
+                data = json.load(f)
+            train_data = data['train_data']
+
 
         with open(meta_data_path, 'r') as f:
             data = json.load(f)
